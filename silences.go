ff92e2a71149b2db171976f981222a7c92f892a8
Matthias Loibl <mail@matthiasloibl.com>
2016-12-10 21:13:53 +0100

    Improve formatting of silences and sort them

--- silences.go
+++ silences.go
@@ -4,10 +4,21 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sort"
 	"strings"
+	"time"
 
+	"github.com/hako/durafmt"
 	"github.com/prometheus/alertmanager/types"
 )
+
+// ByStatus implements sort.Interface for []types.Silence based on
+// the EndsAt field.
+type ByStatus []types.Silence
+
+func (s ByStatus) Len() int           { return len(s) }
+func (s ByStatus) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s ByStatus) Less(i, j int) bool { return !s[i].EndsAt.Before(s[j].EndsAt) }
 
 type silencesResponse struct {
 	Data   []types.Silence `json:"data"`
@@ -28,12 +39,15 @@ func listSilences(c Config) ([]types.Silence, error) {
 		return nil, err
 	}
 
-	return silencesResponse.Data, err
+	silences := silencesResponse.Data
+	sort.Sort(ByStatus(silences))
+
+	return silences, err
 }
 
 // SilenceMessage converts a silences to a message string
 func SilenceMessage(s types.Silence) string {
-	var alertname, matchers string
+	var alertname, emoji, matchers, duration string
 
 	for _, m := range s.Matchers {
 		if m.Name == "alertname" {
@@ -43,9 +57,34 @@ func SilenceMessage(s types.Silence) string {
 		}
 	}
 
+	resolved := Resolved(s)
+	if !resolved {
+		emoji = " 🔕"
+		duration = fmt.Sprintf(
+			"*Started*: %s ago\n*Ends:* %s\n",
+			durafmt.Parse(time.Since(s.StartsAt)),
+			durafmt.Parse(time.Since(s.EndsAt)),
+		)
+	} else {
+		duration = fmt.Sprintf(
+			"*Ended*: %s ago\n*Duration*: %s",
+			durafmt.Parse(time.Since(s.EndsAt)),
+			durafmt.Parse(s.EndsAt.Sub(s.StartsAt)),
+		)
+	}
+
 	return fmt.Sprintf(
-		"%s 🔕\n```%s```\n",
-		alertname,
+		"%s%s\n```%s```\n%s\n",
+		alertname, emoji,
 		strings.TrimSpace(matchers),
+		duration,
 	)
+}
+
+// Resolved returns if a silence is reolved by EndsAt
+func Resolved(s types.Silence) bool {
+	if s.EndsAt.IsZero() {
+		return false
+	}
+	return !s.EndsAt.After(time.Now())
 }
